4bf1944f68d81647e84f06b547efc200a4034c40
Northword <northword@outlook.com>
2025-06-11 22:21:19 +0800

    chore: migrate from zotero-plugin-scaffold repo

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// eslint.config.mjs
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  javascript: true,
+  typescript: true,
+  stylistic: {
+    semi: true,
+    quotes: "double",
+  },
+  formatters: true,
+  ignores: ["src/typegen.ts"],
+});
